7bdcdf3f7e59fa967f905c745136f1a96f3378ed
vikas <vchandra.iitk@gmail.com>
2020-09-01 22:29:57 +0530

    2nd commit

---
+++ src/main/java/com/util/org/spark/world/Parallel.java
@@ -0,0 +1,20 @@
+package com.util.org.spark.world;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+
+public class Parallel {
+
+    public static void main(String[] args) {
+        SparkConf sparkConf = new SparkConf().setAppName("Spark RDD foreach Example")
+                .setMaster("local[2]").set("spark.executor.memory","2g");
+        JavaSparkContext sc = new JavaSparkContext(sparkConf);
+        JavaRDD<Integer> rdd = sc.parallelize(Arrays.asList(1,2,3,4,5,6,7,8,89,3,4,4,35,646,4,6,64,64));
+        //rdd.collect().forEach(System.out::println);
+        System.out.println(rdd.getNumPartitions());
+
+    }
+}
---
+++ src/main/java/com/util/org/spark/world/ReadFileExample.java
@@ -0,0 +1,41 @@
+package com.util.org.spark.world;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.List;
+
+public class ReadFileExample {
+
+    public static void main(String[] args) {
+        String s = "/Users/vikaschandra/Downloads/1.csv";
+        SparkConf sparkConf = new SparkConf().setAppName("Spark RDD foreach Example")
+                .setMaster("local[2]").set("spark.executor.memory","2g");
+        JavaSparkContext sc = new JavaSparkContext(sparkConf);
+        JavaRDD<String> rdd = sc.textFile(s);
+
+        List<String> list =  rdd.collect();
+        System.out.println(list.size());
+        list.forEach(System.out::println);
+
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Java Spark SQL basic example")
+                .config("spark.master", "local")
+                .getOrCreate();
+
+        Dataset<Row> df = spark.read().format("com.databricks.spark.csv").option("header", true).load(s);
+        df.show();
+
+        Dataset<Row> df1 = df.filter(df.col("id").equalTo("1"));
+        df1.show();
+
+        Dataset<Row> df2 = df1.drop("Marks");
+        df2.show();
+
+    }
+}
